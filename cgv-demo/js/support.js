0f996818eb513da16a7b2ae7a781ed09bab4cf8f
DK <ehdzb28@naver.com>
2026-04-06 18:27:47 +0900

    2026-04-06-final

--- cgv-demo/js/support.js
+++ cgv-demo/js/support.js
@@ -2,38 +2,16 @@ window.addEventListener('DOMContentLoaded', () => {
     let search_content = document.querySelector('#search_content');
     let btnSearch = document.querySelector('#btnSearch');
 
-    let filterSystem = document.querySelector('#system');
-    let filterTheater = document.querySelector('#theater');
-    let filterEvent = document.querySelector('#event');
-    let filterPartner = document.querySelector('#partner');
-    let filterEtc = document.querySelector('#etc');
-    let filterAll = document.querySelector('#all');
+    let menuList = document.querySelectorAll('.filter-menu li a')
     
-    createTableAll();
+    filterMenu('all');
 
-    // all 타입 출력
-    filterAll.addEventListener('click', ()=>{
-        createTableAll();
-    })
-    // system 타입 출력
-    filterSystem.addEventListener('click', ()=>{
-        createTableSystem();
-    })
-    // theater 타입 출력
-    filterTheater.addEventListener('click', ()=>{
-        createTableTheater();
-    })
-    // event 타입 출력
-    filterEvent.addEventListener('click', ()=>{
-        createTableEvent();
-    })
-    // partner 타입 출력
-    filterPartner.addEventListener('click', ()=>{
-        createTablePartner();
-    })
-    // etc 타입 출력
-    filterEtc.addEventListener('click', ()=>{
-        createTableEtc();
+    menuList.forEach(menu => {
+        menu.addEventListener('click', () => {
+            menuList.forEach(menu => menuDefaultColor(menu))
+            menuSelectColor(menu)
+            filterMenu(menu.id);
+        })
     })
 
     btnSearch.addEventListener('click', () => {
@@ -49,11 +27,22 @@ async function getJson () {
     return response.json();
 }
 
-async function createTableAll () {
+async function filterMenu(type) {
     let jsonData = await getJson();
+    let filterData = null;
+
+    if (type ==='all') {
+        createTable(jsonData);
+    } else {
+        filterData = jsonData.filter(item => item.type == type);
+        createTable(filterData);
+    }
+}
+
+async function createTable (list) {
     let output = `
         ${
-            jsonData.map( (val, idx) =>
+            list.map( (val, idx) =>
                 `<tr>
                     <td>${idx+1}</td>
                     <td>${val.type}</td>
@@ -67,102 +56,14 @@ async function createTableAll () {
     document.querySelector('#table-list').innerHTML = output;
 }
 
-async function createTableSystem () {
-    let jsonData = await getJson();
-    let filterSystem = jsonData.filter(item => item.type == 'system')
-
-    let output = `
-        ${
-            filterSystem.map( (val, idx) =>
-                `<tr>
-                    <td>${idx+1}</td>
-                    <td>${val.type}</td>
-                    <td><a href="#">${val.title}</a></td>
-                    <td>${val.rdate}</td>
-                    <td>${val.hits}</td>
-                </tr>`
-            ).join("")
-        }`
-
-        document.querySelector('#table-list').innerHTML = output;
+function menuDefaultColor(menu) {
+    menu.style.background = "rgb(137,137,135)";
+    menu.style.borderLeft = `1px solid var(rgb(137,137,135))`;
+    menu.style.borderBottom = `2px solid var(rgb(137,137,135))`;
 }
 
-async function createTableTheater () {
-    let jsonData = await getJson();
-    let filterTheater = jsonData.filter(item => item.type == 'theater')
-
-    let output = `
-        ${
-            filterTheater.map( (val, idx) =>
-                `<tr>
-                    <td>${idx+1}</td>
-                    <td>${val.type}</td>
-                    <td><a href="#">${val.title}</a></td>
-                    <td>${val.rdate}</td>
-                    <td>${val.hits}</td>
-                </tr>`
-            ).join("")
-        }`
-
-        document.querySelector('#table-list').innerHTML = output;
-}
-
-async function createTableEvent () {
-    let jsonData = await getJson();
-    let filterEvent = jsonData.filter(item => item.type == 'event')
-
-    let output = `
-        ${
-            filterEvent.map( (val, idx) =>
-                `<tr>
-                    <td>${idx+1}</td>
-                    <td>${val.type}</td>
-                    <td><a href="#">${val.title}</a></td>
-                    <td>${val.rdate}</td>
-                    <td>${val.hits}</td>
-                </tr>`
-            ).join("")
-        }`
-
-        document.querySelector('#table-list').innerHTML = output;
-}
-
-async function createTablePartner () {
-    let jsonData = await getJson();
-    let filterPartner = jsonData.filter(item => item.type == 'partner')
-
-    let output = `
-        ${
-            filterPartner.map( (val, idx) =>
-                `<tr>
-                    <td>${idx+1}</td>
-                    <td>${val.type}</td>
-                    <td><a href="#">${val.title}</a></td>
-                    <td>${val.rdate}</td>
-                    <td>${val.hits}</td>
-                </tr>`
-            ).join("")
-        }`
-
-        document.querySelector('#table-list').innerHTML = output;
-}
-
-async function createTableEtc () {
-    let jsonData = await getJson();
-    let filterEtc = jsonData.filter(item => item.type == 'etc')
-
-    let output = `
-        ${
-            filterEtc.map( (val, idx) =>
-                `<tr>
-                    <td>${idx+1}</td>
-                    <td>${val.type}</td>
-                    <td><a href="#">${val.title}</a></td>
-                    <td>${val.rdate}</td>
-                    <td>${val.hits}</td>
-                </tr>`
-            ).join("")
-        }`
-
-        document.querySelector('#table-list').innerHTML = output;
+function menuSelectColor(menu) {
+    menu.style.background = "rgb(251, 67, 87)";
+    menu.style.borderLeft = `1px solid var(rgb(251, 67, 87))`;
+    menu.style.borderBottom = `2px solid var(rgb(251, 67, 87))`;
 }
